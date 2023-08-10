09c296f1217a1a773ae160a326bdf44bb016775e
PhilipFelipe <wakezinnsaw13@gmail.com>
2023-08-10 15:12:41 -0300

    removed cronjob

--- airport/tasks.py
+++ airport/tasks.py
@@ -1,6 +1,3 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-import logging
-
 from airport.service.airport_service import AirportService
 from airport.external.domestic_airports_api import DomesticAirportAPIConnector
 from airport.repository.airport_repository import AirportRepository
@@ -16,17 +13,3 @@ class AirportTask:
     def update_airports(self):
         airports_data = self.domestic_airport_api_connector.retrieve_airports()
         self.airport_service.update_airports(airports_data)
-
-
-scheduler = BackgroundScheduler()
-
-
-def start_airport_job():
-    def initialize_command():
-        import os
-
-        os.system("python manage.py import_airports")
-
-    scheduler.add_job(initialize_command, "interval", minutes=3)
-    scheduler.start()
-    logging.info("Starting airport api update task.")
