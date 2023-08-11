956e8ff0f20fc0cdbd950ecea5d350e0ebbc0bc9
PhilipFelipe <wakezinnsaw13@gmail.com>
2023-08-11 00:28:16 -0300

    add user, rest_framework token and pt-br to settings

--- flightservice/settings.py
+++ flightservice/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework.authtoken",
     "flight",
     "airport",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -112,9 +114,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pt-br"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Sao_Paulo"
 
 USE_I18N = True
 
