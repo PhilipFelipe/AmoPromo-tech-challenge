3e398ab4720596a26341cd54aa2993b40db561ac
PhilipFelipe <wakezinnsaw13@gmail.com>
2023-08-11 11:46:34 -0300

    fix

--- flight/external/mock_airlines_inc_api.py
+++ flight/external/mock_airlines_inc_api.py
@@ -11,7 +11,7 @@ class MockAirlineAPIConnector:
         self.api_key: str = os.getenv("API_KEY", "")
 
     def get_flights(self, origin: str, destination: str, departure_date: str) -> dict:
-        endpoint = f"https://stub.amopromo.com/air/search/{self.apikey}/{origin}/{destination}/{departure_date}"
+        endpoint = f"https://stub.amopromo.com/air/search/{self.api_key}/{origin}/{destination}/{departure_date}"
         response = requests.get(endpoint, auth=(self.username, self.password))
         response.raise_for_status()
         return response.json()
